f5e14028491e50d3bc4b3274898197b2bfb2dd36
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-01 07:46:47 +0530

    fix: update data fetch URLs

--- Module 5 Solution/js/script.js
+++ Module 5 Solution/js/script.js
@@ -15,11 +15,11 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
-  "https://davids-restaurant.herokuapp.com/categories.json";
+  "https://node-in-docker.onrender.com/categories";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  "https://node-in-docker.onrender.com/menu-items?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
